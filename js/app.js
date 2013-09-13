127d2956733cacc46b8a179f059f99746541e633
Cedric Warny <cedric.warny@gmail.com>
2013-09-12 20:35:47 -0400

    Added a lot of functionality

--- js/app.js
+++ js/app.js
@@ -1,53 +1,31 @@
 var App = Ember.Application.create();
 
 App.Router.map(function() {
-	this.resource('user', {path: '/user/:user_id'});
+	this.resource('visits', function() {
+		this.resource('visit', {path: '/:visit_id'});
+	}),
+	this.route('fetch', {path: '/fetch'});
 });
 
-App.User = DS.Model.extend({
-	first: DS.attr('string'),
-	last: DS.attr('string'),
-	avatar: DS.attr('string')
-})
-
-App.User.adapter = DS.RESTAdapter.extend({
-	url: "http://172.18.18.129:3000"
-	// adapter: App.User.FIXTURES
-});
-
-App.Store = DS.Store.extend({
-	adapter: App.User.adapter.create()
-});
-
-App.ApplicationRoute = Ember.Route.extend({
-	model: function() {
-		return App.User.find();
+App.IndexRoute = Ember.Route.extend({
+	redirect: function() {
+		this.transitionTo("parameters");
 	}
 });
 
-App.UserRoute = Ember.Route.extend({
-	model: function(params) {
-		return App.User.find(params.user_id);
+App.ParametersController = Ember.Controller.extend({
+	startDate: "2013-08-01",
+	endDate: "2013-09-11",
+	profileId: "75218327",
+	actions: {
+		save: function() {
+			$.ajax({
+				type: "POST",
+				url: "http://localhost:3000/fetch",
+				data: { startDate: this.get('startDate'), endDate: this.get('endDate'), profileId: this.get('profileId')}
+			}).done(function(response) {
+				console.log(response);
+			});
+		}
 	}
 });
-
-App.User.FIXTURES = [
-	{
-		id: 0,
-		first: 'Ryan',
-		last: 'Florence',
-		avatar: 'https://si0.twimg.com/profile_images/3123276865/5c069e64eb7f8e971d36a4540ed7cab2.jpeg'
-	},
-	{
-		id: 1,
-		first: 'Tom',
-		last: 'Dale',
-		avatar: 'https://si0.twimg.com/profile_images/1317834118/avatar.png'
-	},
-	{
-		id: 2,
-		first: 'Yehuda',
-		last: 'Katz',
-		avatar: 'https://si0.twimg.com/profile_images/3250074047/46d910af94e25187832cb4a3bc84b2b5.jpeg'
-	}
-];
--- js/server.js
+++ js/server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var fs = require("fs");
 var xml2js = require('xml2js');
 var config = require("./config")
 var tsession = require("temboo/core/temboosession");
@@ -12,10 +13,7 @@ getMetricsInputs.setCredential('GoogleAnalytics');
 
 var app = express();
 
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -29,81 +27,69 @@ var allowCrossDomain = function(req, res, next) {
     }
 };
 
-app.use(allowCrossDomain);
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(allowCrossDomain);
+});
 
-var users = {
-	users: [
-		{
-			id: 0,
-			first: 'Ryan',
-			last: 'Florence',
-			avatar: 'https://si0.twimg.com/profile_images/3123276865/5c069e64eb7f8e971d36a4540ed7cab2.jpeg'
-		},
-		{
-			id: 1,
-			first: 'Tom',
-			last: 'Dale',
-			avatar: 'https://si0.twimg.com/profile_images/1317834118/avatar.png'
-		},
-		{
-			id: 2,
-			first: 'Yehuda',
-			last: 'Katz',
-			avatar: 'https://si0.twimg.com/profile_images/3250074047/46d910af94e25187832cb4a3bc84b2b5.jpeg'
+app.get("/visits", function (req, res){
+	fs.readFile("../api/visits.json", function (err,data) {
+		if (err) throw err;
+		return res.json(JSON.parse(data));
+	});
+});
+
+app.get("/visits/:id", function (req, res) {
+	fs.readFile("../api/visits.json", function (err,data) {
+		if (err) throw err;
+		var visits = JSON.parse(data).visits;
+		if (visits.length <= req.params.id || req.params.id < 0) {
+			res.statusCode = 404;
+			return res.send('Error 404: No visit found');
 		}
-	]
-};
-
-app.get("/users", function(req, res){
-	res.json(users);
+		return res.json({"visit": visits[req.params.id]});
+	});
 });
 
-app.get("/users/:id", function(req, res) {
-	if(users.length <= req.params.id || req.params.id < 0) {
-		res.statusCode = 404;
-		return res.send('Error 404: No user found');
-	}
-
-	var user = users[req.params.id];
-	res.json(user);
-});
-
-app.post("/users", function(req, res) {
-	if(!req.body.hasOwnProperty('first') || !req.body.hasOwnProperty('last') || !req.body.hosOwnProperty('avatar')) {
-		res.statusCode = 400;
-		return res.send('Error 400: Post syntax incorrect.');
-	}
-
-	var newUser = {
-		first : req.body.first,
-		last : req.body.last,
-		avatar : req.body.avatar
-	};
-
-	users.push(newUser);
-	res.json(true);
-});
-
-app.post("/search", function(req,res) {
-
+app.post("/fetch", function (req,res) {
 	getMetricsInputs.set_Filters("ga:source==t.co");
-	getMetricsInputs.set_EndDate("2013-09-11");
-	getMetricsInputs.set_StartDate("2013-08-01");
+	getMetricsInputs.set_EndDate(req.body.endDate);
+	getMetricsInputs.set_StartDate(req.body.startDate);
 	getMetricsInputs.set_Metrics("ga:visits");
 	getMetricsInputs.set_Dimensions("ga:fullReferrer,ga:dateHour");
-	getMetricsInputs.set_ProfileId("75218327");
+	getMetricsInputs.set_ProfileId(req.body.profileId);
 
 	var parser = new xml2js.Parser();
 
 	getMetricsChoreo.execute(
 	    getMetricsInputs,
-	    function(results){
+	    function (results) {
 	    	var xml = results.get_Response();
-	    	parser.parseString(xml, function(err,result) {
-	    		res.json(result);
-	    	})
+	    	parser.parseString(xml, function (err,result) {
+
+	    		var newVisits = [];
+	    		for (var entry in result.feed.entry) {
+	    			var fullReferrer = entry["dxp:dimension"][0].$.value;
+	    			var dateHour = entry["dxp:dimension"][1].$.value;
+	    			var visits = entry["dxp.metric"][0].$.value;
+	    			newVisits.push({fullReferrer:fullReferrer, dateHour:dateHour, visits:visits});
+	    		}
+
+	    		fs.readFile("../api/visits.json", function (err,data) {
+	    			if (err) throw err;
+	    			var visits = JSON.parse(data).visits;
+	    			visits = visits.concat(newVisits);
+
+	    			fs.writeFile("../api/visits.json", JSON.stringify(visits), function (err) {
+	    				if (err) throw err;
+						console.log("Saved");
+						res.json(true);
+	    			});
+	    		});
+	    	});
 	    },
-	    function(error){
+	    function (error) {
 	    	res.send(error.type); res.send(error.message);
 	    }
 	);
