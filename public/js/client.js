6c2140ad6db70c87740410caff8c21e09b71cd28
Cedric Warny <cedric.warny@gmail.com>
2013-12-07 21:34:45 -0500

    Changed urls

--- public/js/client.js
+++ public/js/client.js
@@ -150,7 +150,7 @@ App.SignonButtonView = App.ButtonView.extend({
 		else return false;
 	}.property("controller.controllers.application.model.user"),
 
-	href: "http://localhost:3000/auth/google"
+	href: "http://socialr.herokuapp.com/auth/google"
 });
 
 App.ConnectView = App.SignupStepView.extend({
@@ -166,7 +166,7 @@ App.ConnectButtonView = App.ButtonView.extend({
 		else return false;
 	}.property("controller.controllers.application.model.user"),
 	
-	href: "http://localhost:3000/connect/twitter"
+	href: "http://socialr.herokuapp.com/connect/twitter"
 });
 
 App.GetDataView = App.SignupStepView.extend({
