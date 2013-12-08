dca1656b5814e46bc43ab6d842d76b5f6e8e8b52
Cedric Warny <cedric.warny@gmail.com>
2013-12-08 01:40:35 -0500

    forced approval prompt

--- server.js
+++ server.js
@@ -123,7 +123,8 @@ app.get("/auth/google",
 	passport.authenticate("google", 
 		{ 
 			scope: ["https://www.googleapis.com/auth/userinfo.profile","https://www.googleapis.com/auth/userinfo.email","https://www.googleapis.com/auth/analytics.readonly"],
-			accessType: "offline"
+			accessType: "offline",
+			approvalPrompt: "force"
 		}),
 	function (req, res) {
 
