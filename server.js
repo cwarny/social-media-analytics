8533ae9c4b29c4f28b0c2e01ba0683fd8f21b3e5
Cedric Warny <cedric.warny@gmail.com>
2013-12-09 14:47:54 -0500

    Changed scheduled job to run every hour instead of everyday --for faster debugging

--- server.js
+++ server.js
@@ -186,7 +186,7 @@ app.get("/data", function (req, res) {
 
 // Every day, update user data
 
-schedule.scheduleJob({hour: 19, minute: 25}, function () {
+schedule.scheduleJob({minute: 25}, function () {
 	console.log("Update started...");
 	users.find().toArray(function (err, results) {
 		async.each(results, function (user, cb) {
