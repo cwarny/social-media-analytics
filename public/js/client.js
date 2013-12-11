c55eff739ad64a25c7c135b132178d65bde64f76
Cedric Warny <cedric.warny@gmail.com>
2013-12-11 15:37:45 -0500

    Handle the no ga accounts case

--- public/js/client.js
+++ public/js/client.js
@@ -219,7 +219,7 @@ App.AccountsRoute = Ember.Route.extend({
 		}
 	},
 	afterModel: function (model, transition) {
-		console.log(this.get("model"));
+		console.log(model);
 		if (this.controllerFor("application").get("model.user") && this.controllerFor("application").get("model.user.new")) {
 			this.controllerFor("signup").set("error","Signup process unfinished.");
 			this.transitionTo("signup"); 
