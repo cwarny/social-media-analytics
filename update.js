2c1f0137d967821ec2822df84acc3e432db1b2c2
Cedric Warny <cedric.warny@gmail.com>
2013-12-25 13:04:38 -0500

    Require twitter tokens for trying update

--- update.js
+++ update.js
@@ -23,77 +23,81 @@ mongodb.MongoClient.connect(MONGODB_URI, function (err, database) {
 	users.find().toArray(function (err, results) {
 		async.each(results, function (user, cb) {
 			console.log("User id: " + user.id);
-			refresh(user.refresh_token_google, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, function (err, json, res) {
-				if (err || json.error) cb(err);
-				else {
-					user.access_token_google = json.accessToken;
-					fetchData(user, function (err, results) {
-						async.each(results, function (account, cb) {
-							console.log("Account id: " + account.id);
-							accounts.find({id: account.id}).toArray(function (err, results) {
-								if (!err && results.length > 0 && account.hasOwnProperty("webproperties")) {
-									async.each(account.webproperties, function (webproperty, cb) {
-										console.log("Webproperty id: " + webproperty.id);
-										if (webproperty.hasOwnProperty("profiles") && uu.contains(uu.pluck(results[0].webproperties, "id"), webproperty.id)) {
-											async.map(webproperty.profiles, function (profile, cb) {
-												if (uu.contains(uu.pluck(uu.findWhere(results[0].webproperties, {id: webproperty.id}).profiles, "id"), profile.id)) {
-													var nonupdated_profile = uu.findWhere(uu.findWhere(results[0].webproperties, {id: webproperty.id}).profiles, {id: profile.id});
-													if (profile.hasOwnProperty("referrers")) {
-														console.log("Profile id: " + profile.id);
-														var old_referrers = nonupdated_profile.referrers;
-														if (old_referrers === undefined) old_referrers = [];
-														var new_referrers = profile.referrers;
-														new_referrers = new_referrers.filter(function (referrer) {
-															var r = uu.findWhere(old_referrers, {fullreferrer: referrer.fullreferrer});
-															if (r) {
-																referrer.clicks.forEach(function (c) {
-																	if (!uu.contains(uu.pluck(r.clicks, "created_at"), c.created_at)) {
-																		r.clicks.push(c);
-																	}
-																});
-																return false;
-															} else {
-																return true;
-															}
-														});
-														console.log("Number of new referrers: " + new_referrers.length);
-														profile.referrers = uu.union(old_referrers,new_referrers);
-														cb(null, profile);
+			if (user.hasOwnProperty("twitter_tokens")) {
+				refresh(user.refresh_token_google, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, function (err, json, res) {
+					if (err || json.error) cb(err);
+					else {
+						user.access_token_google = json.accessToken;
+						fetchData(user, function (err, results) {
+							async.each(results, function (account, cb) {
+								console.log("Account id: " + account.id);
+								accounts.find({id: account.id}).toArray(function (err, results) {
+									if (!err && results.length > 0 && account.hasOwnProperty("webproperties")) {
+										async.each(account.webproperties, function (webproperty, cb) {
+											console.log("Webproperty id: " + webproperty.id);
+											if (webproperty.hasOwnProperty("profiles") && uu.contains(uu.pluck(results[0].webproperties, "id"), webproperty.id)) {
+												async.map(webproperty.profiles, function (profile, cb) {
+													if (uu.contains(uu.pluck(uu.findWhere(results[0].webproperties, {id: webproperty.id}).profiles, "id"), profile.id)) {
+														var nonupdated_profile = uu.findWhere(uu.findWhere(results[0].webproperties, {id: webproperty.id}).profiles, {id: profile.id});
+														if (profile.hasOwnProperty("referrers")) {
+															console.log("Profile id: " + profile.id);
+															var old_referrers = nonupdated_profile.referrers;
+															if (old_referrers === undefined) old_referrers = [];
+															var new_referrers = profile.referrers;
+															new_referrers = new_referrers.filter(function (referrer) {
+																var r = uu.findWhere(old_referrers, {fullreferrer: referrer.fullreferrer});
+																if (r) {
+																	referrer.clicks.forEach(function (c) {
+																		if (!uu.contains(uu.pluck(r.clicks, "created_at"), c.created_at)) {
+																			r.clicks.push(c);
+																		}
+																	});
+																	return false;
+																} else {
+																	return true;
+																}
+															});
+															console.log("Number of new referrers: " + new_referrers.length);
+															profile.referrers = uu.union(old_referrers,new_referrers);
+															cb(null, profile);
+														} else {
+															cb(null, nonupdated_profile);
+														}
 													} else {
-														cb(null, nonupdated_profile);
+														console.log("New profile");
+														cb(null, profile);
 													}
-												} else {
-													console.log("New profile");
-													cb(null, profile);
-												}
-											}, function (err, profiles) {
-												accounts.update({"webproperties.id":webproperty.id},{$set:{"webproperties.$.profiles": profiles}}, function (err, results) {
-													console.log("Webproperty updated.");
+												}, function (err, profiles) {
+													accounts.update({"webproperties.id":webproperty.id},{$set:{"webproperties.$.profiles": profiles}}, function (err, results) {
+														console.log("Webproperty updated.");
+														cb(err);
+													});
+												});
+											} else {
+												accounts.update({id: account.id}, {$push: {webproperties: webproperty}}, function (err, results) {
+													console.log("New webproperty saved.");
 													cb(err);
 												});
-											});
-										} else {
-											accounts.update({id: account.id}, {$push: {webproperties: webproperty}}, function (err, results) {
-												console.log("New webproperty saved.");
-												cb(err);
-											});
-										}
-									}, function (err) {
-										cb(err);
-									});
-								} else {
-									accounts.save(account, function (err, res) {
-										console.log("New account saved.");
-										cb(err);
-									});
-								}
+											}
+										}, function (err) {
+											cb(err);
+										});
+									} else {
+										accounts.save(account, function (err, res) {
+											console.log("New account saved.");
+											cb(err);
+										});
+									}
+								});
+							}, function (err) {
+								cb(err);
 							});
-						}, function (err) {
-							cb(err);
 						});
-					});
-				}
-			});
+					}
+				});
+			} else {
+				cb(null);
+			}
 		}, function (err) {
 			console.log("Done updating.");
 		});
